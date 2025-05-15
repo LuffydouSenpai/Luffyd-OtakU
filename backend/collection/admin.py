e68424aedb9fda8a93ed47979cc9a43c657699c6
wbello <williams.bello@outlook.fr>
2025-05-15 09:51:36 +0200

    maj 14 mai

--- backend/collection/admin.py
+++ backend/collection/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.utils.html import format_html
-from .models import Anime, Genre, Theme, AnimeImage, AnimeTitle, RelationAnime
+from .models import Anime, Genre, Theme, AnimeImage, AnimeTitle, RelationAnime, Manga, MangaImage, MangaTitle, RelationManga, Publisher, TypeManga, Language, RelationType, Origin, Format, Season
 from .forms import AnimeForm
 
 
@@ -18,14 +18,51 @@ class ThemeAdmin(admin.ModelAdmin):
     search_fields = ('name',)
     exclude = ('slug',)  # Exclut le champ 'slug' du formulaire
     
+@admin.register(Publisher)
+class PublisherAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name',)
+    exclude = ('slug',)  # Exclut le champ 'slug' du formulaire
     
+@admin.register(TypeManga)
+class TypeMangaAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name',)
+    exclude = ('slug',)  # Exclut le champ 'slug' du formulaire
+    
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name')
+    search_fields = ('name',)
+    
+@admin.register(RelationType)
+class RelationTypeAdmin(admin.ModelAdmin):
+    list_display = ('code', 'label', 'inverse')  # visible dans la liste
+    search_fields = ('code', 'label')            # champ de recherche
+    list_filter = ('inverse',)                   # filtre latéral
+    
+@admin.register(Origin)
+class OriginAdmin(admin.ModelAdmin):
+    list_display = ('code', 'label')  # visible dans la liste
+    search_fields = ('code', 'label')            # champ de recherche
+    
+@admin.register(Format)
+class FormatAdmin(admin.ModelAdmin):
+    list_display = ('code', 'label')  # visible dans la liste
+    search_fields = ('code', 'label')            # champ de recherche
+
+@admin.register(Season)
+class SeasonAdmin(admin.ModelAdmin):
+    list_display = ('season', 'year')  # visible dans la liste
+    search_fields = ('season', 'year')            # champ de recherche
+
+
 
 # anime
 @admin.register(Anime)
 class AnimeAdmin(admin.ModelAdmin):
 
     form = AnimeForm  # Associe le formulaire personnalisé à l'admin
-    
     
     list_display = ('id', 'main_title', 'image_previews', 'slug')
     search_fields = ['main_title']
@@ -80,9 +117,9 @@ class AnimeTitleAdmin(admin.ModelAdmin):
         print(f"Recherche avec le terme : {search_term}")
         return super().get_search_results(request, queryset, search_term)
     
-    list_display = ('anime', 'title', 'language_code', 'is_original', 'is_nickname','is_main', 'slug')
-    search_fields = ('title', 'language_code', 'slug')
-    list_filter = ('is_original', 'is_nickname', 'language_code')
+    list_display = ('anime', 'title', 'language', 'is_original', 'is_nickname','is_main', 'slug')
+    search_fields = ('title', 'language', 'slug')
+    list_filter = ('is_original', 'is_nickname', 'language')
     exclude = ('slug',)  # Exclut le champ 'slug' du formulaire
     
 @admin.register(RelationAnime)
@@ -98,3 +135,78 @@ class RelationAnimeAdmin(admin.ModelAdmin):
 
 
 #manga
+@admin.register(Manga)
+class MangaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'main_title', 'image_previews', 'slug')
+    search_fields = ['main_title']
+    list_filter = ('genres', 'themes')
+    filter_horizontal = ('genres', 'themes')  # meilleure UX pour les ManyToMany
+    exclude = ('slug',)  # Exclut le champ 'slug' du formulaire
+    
+    def save_model(self, request, obj, form, change):
+    # Sauvegarde de l'objet Manga
+        super().save_model(request, obj, form, change)
+
+        # Crée un MangaTitle automatiquement si on est en création et qu'un titre est fourni
+        if not change and 'main_title' in form.cleaned_data:
+            try:
+                lang_fr = Language.objects.get(code='fr')  # Récupère la langue 'fr'
+            except Language.DoesNotExist:
+                lang_fr = None  # Ou gérer autrement si c’est bloquant
+
+            if lang_fr:  # On ne crée que si la langue existe
+                MangaTitle.objects.create(
+                    manga=obj,
+                    title=form.cleaned_data['main_title'],
+                    language=lang_fr,
+                    is_main=True  # Si le champ existe
+                )
+        
+    def image_previews(self, obj):
+        images = obj.images.all()  # Récupère toutes les images de cet anime
+        preview_html = ''
+        for image in images:
+            preview_html += f'<img src="{image.image.url}" width="75" height="100" style="margin-right:5px;" />'
+        return format_html(preview_html) if images else 'Aucune image'
+        
+    image_previews.allow_tags = True  # Permet d'afficher le HTML dans l'admin
+    image_previews.short_description = 'Aperçu des images'  # Titre de la colonne
+
+@admin.register(MangaImage)
+class MangaImageAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['manga']
+    list_display = ('manga', 'description', 'image_preview')
+    search_fields = ('manga__main_title', 'description')
+    list_filter = ('manga',)
+
+    def image_preview(self, obj):
+        return format_html(
+                '<img src="{}" width="75" height="100" style="margin-right:5px;" />',
+                obj.image.url
+            )
+    image_preview.allow_tags = True  # Permet d'afficher le HTML dans l'admin
+    image_preview.short_description = 'Aperçu de l\'image'
+
+@admin.register(MangaTitle)
+class MangaTitleAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['manga']
+    search_fields = ['manga__main_title']
+    
+    def get_search_results(self, request, queryset, search_term):
+        print(f"Recherche avec le terme : {search_term}")
+        return super().get_search_results(request, queryset, search_term)
+    
+    list_display = ('manga', 'title', 'language', 'is_original', 'is_nickname','is_main', 'slug')
+    search_fields = ('title', 'language', 'slug')
+    list_filter = ('is_original', 'is_nickname', 'language')
+    exclude = ('slug',)  # Exclut le champ 'slug' du formulaire
+
+@admin.register(RelationManga)
+class RelationMangaAdmin(admin.ModelAdmin):
+    list_display = ('manga_source', 'relation_type', 'manga_target')
+    search_fields = (
+        'manga_source__title',
+        'manga_target__title',
+        'relation_type',
+    )
+    autocomplete_fields = ('manga_source', 'manga_target')
--- backend/collection/models.py
+++ backend/collection/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from slugify import slugify  # pip install python-slugify
 import os
 
-#genre et theme
+#table unique
 class Genre(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.CharField(max_length=50)
@@ -28,46 +28,91 @@ class Theme(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Publisher(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.CharField(max_length=50)
+    
+    def save(self, *args, **kwargs):
+        original_slug = slugify(f"{self.name}")
+        slug = original_slug
+        self.slug = slug
+        super().save(*args, **kwargs)
 
+    def __str__(self):
+        return self.name
 
+class TypeManga(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.CharField(max_length=50)
+    
+    def save(self, *args, **kwargs):
+        original_slug = slugify(f"{self.name}")
+        slug = original_slug
+        self.slug = slug
+        super().save(*args, **kwargs)
 
+    def __str__(self):
+        return self.name
+    
+class Language(models.Model):
+    code = models.CharField(max_length=5, unique=True)  # 'jp'
+    name = models.CharField(max_length=50)  # 'Japonais'
+    native_name = models.CharField(max_length=50)  # '日本語'
+
+    def __str__(self):
+        return self.name
+
+class RelationType(models.Model):
+    code = models.CharField(max_length=50, unique=True)
+    label = models.CharField(max_length=100)
+    inverse = models.ForeignKey(
+        'self',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='inverse_of'
+    )
+
+    def __str__(self):
+        return self.label
+    
+class Origin(models.Model):
+    code = models.CharField(max_length=50, unique=True)
+    label = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.label
+    
+class Format(models.Model):
+    code = models.CharField(max_length=50, unique=True)
+    label = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.label
+
+class Season(models.Model):
+    SEASON_CHOICES = [
+        ('winter', 'Hiver'),
+        ('spring', 'Printemps'),
+        ('summer', 'Été'),
+        ('autumn', 'Automne'),
+    ]
+
+    season = models.CharField(max_length=10, choices=SEASON_CHOICES)
+    year = models.PositiveIntegerField()
+
+    class Meta:
+        unique_together = ('season', 'year')
+
+    def __str__(self):
+        return f"{dict(self.SEASON_CHOICES).get(self.season)} {self.year}"
 
 # anime
 class Anime(models.Model):
-    ORIGIN_CHOICES = [
-        ('original', '	Œuvre originale'),
-        ('manga', 'Manga'),
-        ('light-novel', 'Light Novel'),
-        ('visual-novel', 'Visual Novel'),
-        ('jeu-video', 'Jeu vidéo'),
-        ('Jouets-/-figurines', 'Jouets / Figurines'),
-        ('roman', 'Roman'),
-        ('jeu-de-carte', 'Jeu de carte'),
-        ('vocaloid', 'Vocaloid'),
-        ('projet-transmedia', 'Projet transmédia'),
-        ('autre', 'Autre'),
-        ('anime', 'Anime'),
-        ('doujinshi', 'Doujinshi'),
-        ('film', 'Film'),
-        ('chanson', 'Chanson'),
-        ('vtuber', 'Vtuber'),
-    ]
-    
-    FORMAT_CHOICES = [
-        ('serie', 'Série'),
-        ('oav', 'OAV'),
-        ('film', 'Film'),
-        ('special', 'Spécial'),
-        ('musique', 'Musique'),
-        ('animix-/-picture-drama', 'Animix / Picture drama'),
-        ('court-metrage', 'Court-métrage'),
-        ('publicité', 'Publicité'),
-        ('inconnu', 'inconnu'),
-        ('bonus', 'Bonus'),
-    ]
     
     main_title = models.CharField(max_length=255)
-    origin = models.CharField(max_length=50, choices=ORIGIN_CHOICES)
+    origin = models.ForeignKey(Origin, null=True, on_delete=models.CASCADE, related_name='animes')
     synopsis = models.TextField()
     date_start = models.DateField(null=True, blank=True)
     date_end = models.DateField(null=True, blank=True)
@@ -76,7 +121,7 @@ class Anime(models.Model):
     season = models.CharField(max_length=50, null=True, blank=True)
     studio = models.CharField(max_length=150, null=True, blank=True)
     studio_3D = models.CharField(max_length=150, null=True, blank=True)
-    format = models.CharField(max_length=50, choices=FORMAT_CHOICES)
+    format = models.ForeignKey(Format, null=True, on_delete=models.CASCADE, related_name='animes')
     url_nautiljon = models.TextField()
     url_mal = models.TextField()
     genres = models.ManyToManyField('Genre', related_name='animes')
@@ -131,17 +176,10 @@ class AnimeImage(models.Model):
         return f"Image de {self.anime.main_title}"
     
 class AnimeTitle(models.Model):
-    CODE_CHOICES = [
-        ('jp', 'jp'),
-        ('en', 'en'),
-        ('fr', 'fr'),
-        ('ch', 'ch'),
-    ]
-    
     
     anime = models.ForeignKey(Anime, related_name='anime_titles', on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
-    language_code = models.CharField(max_length=2, choices=CODE_CHOICES)  # Code de la langue (ex: 'en' pour anglais, 'ja' pour japonais)
+    language = models.ForeignKey(Language, null=True, on_delete=models.PROTECT)
     is_original = models.BooleanField(default=False)  # Indique si le titre est original (true) ou une version modifiée (false)
     is_nickname = models.BooleanField(default=False)  # Indique si le titre est un surnom ou un autre alias
     is_main = models.BooleanField(default=False)      # Indique si le titre celui que j'utiliserais principalement
@@ -150,7 +188,7 @@ class AnimeTitle(models.Model):
     class Meta:
         # Contrainte d'unicité pour assurer qu'il n'y ait pas de doublon
         constraints = [
-            models.UniqueConstraint(fields=['anime', 'title', 'language_code'], name='unique_anime_title_language')
+            models.UniqueConstraint(fields=['anime', 'title', 'language'], name='unique_anime_title_language')
         ]
         
         
@@ -170,33 +208,9 @@ class AnimeTitle(models.Model):
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"Title: {self.title} ({self.language_code}) - Original: {self.is_original}"
+        return f"Title: {self.title} ({self.language}) - Original: {self.is_original}"
     
 class RelationAnime(models.Model):
-    RELATION_CHOICES = [
-        ('suite', 'Suite'),
-        ('prequel', 'Préquelle'),
-        ('spin-off', 'Spin-off'),
-        ('remake', 'Remake'),
-        ('histoire-supplémentaire', 'Histoire supplémentaire'),
-        ('version-alternative', 'Version alternative'),
-        ('compilation', 'Compilation'),
-        ('parodie', 'Parodie'),
-        ('pilote', 'Pilote')
-        # Tu peux en ajouter d’autres ici
-    ]
-
-    INVERSE_RELATIONS = {
-        'suite': 'prequel',
-        'prequel': 'suite',
-        'spin-off': 'origine-du-spin-off',
-        'remake': 'origine-du-remake',  # symétrique
-        'histoire-supplémentaire': 'histoire-d-origine',
-        'version-alternative': 'version-alternative',
-        'compilation': 'histoire-complete',
-        'parodie': 'origine-de-la-parodie',
-        'pilote': 'version-finale'
-    }
 
     anime_source = models.ForeignKey(
         'Anime',
@@ -208,10 +222,13 @@ class RelationAnime(models.Model):
         on_delete=models.CASCADE,
         related_name='relations_as_target'
     )
-    relation_type = models.CharField(max_length=50, choices=RELATION_CHOICES)
+    relation_type = models.ForeignKey(
+        RelationType,
+        on_delete=models.CASCADE
+    )
 
     class Meta:
-        unique_together = ('anime_source', 'anime_target', 'relation_type')
+        unique_together = ('anime_source', 'anime_target')
 
     def __str__(self):
         return f"{self.anime_source} → {self.relation_type} → {self.anime_target}"
@@ -219,12 +236,11 @@ class RelationAnime(models.Model):
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
-        inverse_type = self.INVERSE_RELATIONS.get(self.relation_type)
+        inverse_type = self.relation_type.inverse
 
-        # Vérifie si l’inverse n’existe pas déjà
         if inverse_type and not RelationAnime.objects.filter(
             anime_source=self.anime_target,
-            anime_target=self.anime_source,
+            manga_target=self.anime_source,
             relation_type=inverse_type
         ).exists():
             RelationAnime.objects.create(
@@ -232,10 +248,9 @@ class RelationAnime(models.Model):
                 anime_target=self.anime_source,
                 relation_type=inverse_type
             )
-            
+
     def delete(self, *args, **kwargs):
-        inverse_type = self.INVERSE_RELATIONS.get(self.relation_type)
-        # Supprime la relation inverse si elle existe
+        inverse_type = self.relation_type.inverse
         if inverse_type:
             RelationAnime.objects.filter(
                 anime_source=self.anime_target,
@@ -247,3 +262,153 @@ class RelationAnime(models.Model):
 
 
 #manga  
+class Manga(models.Model):
+    
+    main_title = models.CharField(max_length=255)
+    type = models.ForeignKey(TypeManga, null=True, on_delete=models.CASCADE, related_name='mangas')
+    synopsis = models.TextField()
+    year_start_jp = models.PositiveIntegerField()
+    year_start_fr = models.PositiveIntegerField()
+    publisher = models.ForeignKey(Publisher, null=True, on_delete=models.CASCADE, related_name='mangas')
+    author = models.CharField(max_length=150, null=True, blank=True)
+    scenariste = models.CharField(max_length=150, null=True, blank=True)
+    designer = models.CharField(max_length=150, null=True, blank=True)
+    chara_designer = models.CharField(max_length=150, null=True, blank=True)
+    storage = models.CharField(max_length=50)
+    url_nautiljon = models.TextField()
+    url_mal = models.TextField()
+    genres = models.ManyToManyField('Genre', related_name='mangas')
+    themes = models.ManyToManyField('Theme', related_name='mangas')
+    slug = models.CharField(max_length=255)  # Pour une URL simplifiée du titre, souvent utilisée pour les routes
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            original_slug = slugify(f"{self.main_title}")
+            slug = original_slug
+            counter = 1
+
+            # Si le slug existe déjà, ajouter un compteur
+            while AnimeTitle.objects.filter(slug=slug).exists():
+                slug = f"{original_slug}-{counter}"
+                counter += 1
+
+            self.slug = slug
+
+        super().save(*args, **kwargs)
+        
+    def __str__(self):
+        return f"{self.main_title}"
+       
+class MangaImage(models.Model):
+    manga = models.ForeignKey(Manga, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='manga_images/')
+    description = models.CharField(max_length=255, blank=True)
+    
+    def save(self, *args, **kwargs):
+        # Générer le nom de fichier basé sur le slug de l'anime
+        manga_slug = slugify(self.manga.slug)
+        base_filename = f"{manga_slug}"
+
+        # Vérifier s'il existe déjà une image avec ce nom (pour ajouter un suffixe)
+        count = 1
+        while AnimeImage.objects.filter(image__startswith=base_filename).exists():
+            base_filename = f"{manga_slug}-{count}"
+            count += 1
+        
+        # Renommer l'image avant de la sauvegarder
+        if self.image:
+            # Récupérer le nom de fichier original (extension)
+            ext = os.path.splitext(self.image.name)[1]
+            # Créer un nouveau chemin pour l'image
+            new_image_name = f"{base_filename}{ext}"
+            self.image.name = new_image_name  # Mettre à jour le nom du fichier
+
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return f"Image de {self.manga.main_title}"
+    
+class MangaTitle(models.Model):
+    
+    manga = models.ForeignKey(Manga, related_name='manga_titles', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    language = models.ForeignKey(Language, null=True, on_delete=models.PROTECT)
+    is_original = models.BooleanField(default=False)  # Indique si le titre est original (true) ou une version modifiée (false)
+    is_nickname = models.BooleanField(default=False)  # Indique si le titre est un surnom ou un autre alias
+    is_main = models.BooleanField(default=False)      # Indique si le titre celui que j'utiliserais principalement
+    slug = models.CharField(max_length=255)  # Pour une URL simplifiée du titre, souvent utilisée pour les routes
+    
+    class Meta:
+        # Contrainte d'unicité pour assurer qu'il n'y ait pas de doublon
+        constraints = [
+            models.UniqueConstraint(fields=['manga', 'title', 'language'], name='unique_manga_title_language')
+        ]
+        
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            original_slug = slugify(f"{self.title}")
+            slug = original_slug
+            counter = 1
+
+            # Si le slug existe déjà, ajouter un compteur
+            while MangaTitle.objects.filter(slug=slug).exists():
+                slug = f"{original_slug}-{counter}"
+                counter += 1
+
+            self.slug = slug
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Title: {self.title} ({self.language}) - Original: {self.is_original}"
+      
+class RelationManga(models.Model):
+    
+    manga_source = models.ForeignKey(
+        'Manga',
+        on_delete=models.CASCADE,
+        related_name='relations_as_source'
+    )
+    manga_target = models.ForeignKey(
+        'Manga',
+        on_delete=models.CASCADE,
+        related_name='relations_as_target'
+    )
+    relation_type = models.ForeignKey(
+        RelationType,
+        on_delete=models.CASCADE
+    )
+    
+    class Meta:
+        unique_together = ('manga_source', 'manga_target')
+        
+    def __str__(self):
+        return f"{self.manga_source} → {self.relation_type} → {self.manga_target}"
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        inverse_type = self.relation_type.inverse
+
+        if inverse_type and not RelationManga.objects.filter(
+            manga_source=self.manga_target,
+            manga_target=self.manga_source,
+            relation_type=inverse_type
+        ).exists():
+            RelationManga.objects.create(
+                manga_source=self.manga_target,
+                manga_target=self.manga_source,
+                relation_type=inverse_type
+            )
+
+    def delete(self, *args, **kwargs):
+        inverse_type = self.relation_type.inverse
+        if inverse_type:
+            RelationManga.objects.filter(
+                manga_source=self.manga_target,
+                manga_target=self.manga_source,
+                relation_type=inverse_type
+            ).delete()
+        super().delete(*args, **kwargs)
+    
+    
